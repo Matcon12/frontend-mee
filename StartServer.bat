448622a2a0375ef055622ef3256c658e6743fd6c
Matcon12 <147035101+Matcon12@users.noreply.github.com>
2023-11-21 14:52:52 +0530

    Add files via upload

---
+++ StartServer.bat
@@ -0,0 +1,7 @@
+@echo off
+cd "C:\Users\matcon\Downloads\mee-material-management"
+start cmd.exe /k "call npx kill-port 3000"
+
+cd "C:\Users\matcon\Downloads\mee-material-management"
+start cmd.exe /k "call npm start"
+pause
---
+++ clone_and_run.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Cloning the React.js repository...
+git clone https://github.com/Matcon12/mee-material-management.git
+
+cd 'mee-material-management'
+
+echo Installing npm packages...
+npm install
