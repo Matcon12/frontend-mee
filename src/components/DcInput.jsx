63bd45854881646aff166857a918941887adfe61
Matcon12 <matcon545@gmail.com>
2024-01-02 11:53:27 +0530

    "corrected dc"

--- src/components/DcInput.jsx
+++ src/components/DcInput.jsx
@@ -17,9 +17,9 @@ function DcInput() {
     // Check if the current date is before April 1st
     const cur = today.getFullYear();
     if (currentMonth < 4) {
-      return "/" + today.getFullYear() - 1 + "-" + (cur % 100);
+      return "/" + (cur - 1) + "-" + (cur % 100);
     } else {
-      return "/" + today.getFullYear() + "-" + ((cur + 1) % 100);
+      return "/" + cur + "-" + ((cur + 1) % 100);
     }
   }
 
@@ -28,12 +28,12 @@ function DcInput() {
 
     var gcn_no = document.getElementsByName("gcn_no")[0]?.value;
     var len = gcn_no.length;
-    if (gcn_no[len - 1] == "R") {
-      if (len == 3) gcn_no = "0" + gcn_no;
-      else if (len == 2) gcn_no = "00" + gcn_no;
+    if (gcn_no[len - 1] === "R") {
+      if (len === 3) gcn_no = "0" + gcn_no;
+      else if (len === 2) gcn_no = "00" + gcn_no;
     } else {
-      if (len == 2) gcn_no = "0" + gcn_no;
-      else if (len == 1) gcn_no = "00" + gcn_no;
+      if (len === 2) gcn_no = "0" + gcn_no;
+      else if (len === 1) gcn_no = "00" + gcn_no;
     }
     var gcn = gcn_no + document.getElementsByName("fin_year")[0]?.value;
     navigate(`/dc-printing?dc_no=${gcn}`);
